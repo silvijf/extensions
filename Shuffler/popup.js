b1ea7773dcac444f5c421b126c672ce6fa367fb0
Silvijn de Haas <silvijf38@gmail.com>
2026-01-15 17:27:37 +0100

    Update notifications!

--- Shuffler/popup.js
+++ Shuffler/popup.js
@@ -1,8 +1,10 @@
 const config = document.getElementById("config");
 const shuffleKeyEl = document.getElementById("shuffleKey");
-const warning = document.getElementById("warning");
+const refresh = document.getElementById("refresh");
+const newVersion = document.getElementById("newversion")
 let configuring = false;
 let block = false;
+
 chrome.storage.local.get("shuffleKey", (result) => {
     if (result.shuffleKey == undefined) chrome.storage.local.set({ shuffleKey: "=" });
     else shuffleKeyEl.innerText = (result.shuffleKey == " ") ? "Space" : result.shuffleKey;
@@ -28,7 +30,18 @@ document.addEventListener("keyup", (e) => {
         shuffleKeyEl.innerText = (e.key == " ") ? "Space" : e.key;
         config.innerText = "Configure shuffle key...";
         configuring = false;
-        warning.style.display = "block";
+        refresh.style.display = "block";
     }
     block = false;
 })
+
+async function getVersion() {
+    let file = await fetch("https://raw.githubusercontent.com/silvijf/extensions/refs/heads/main/Shuffler/manifest.json");
+    let text = await file.text();
+    let startIndex = text.indexOf('"version"') + 12;
+    let endIndex = text.indexOf('"', startIndex);
+    console.log(text.substring(startIndex, endIndex), chrome.runtime.getManifest().version)
+    if (text.substring(startIndex, endIndex) != chrome.runtime.getManifest().version) newVersion.style.display = "block";
+}
+
+getVersion()
