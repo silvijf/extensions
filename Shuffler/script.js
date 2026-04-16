890d11dc30b32dfd44ff73bc599a45cfdb83377c
Silvijn de Haas <silvijf38@gmail.com>
2026-04-16 16:58:50 +0200

    [2.5] Change the way shuffleable elements are selected

--- Shuffler/script.js
+++ Shuffler/script.js
@@ -8,38 +8,38 @@ chrome.storage.local.get("shuffleKey", (result) => {
 })
 
 function shuffle() {
-    let string = selectedElement.textContent.split("")
-    for (let i = string.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [string[i], string[j]] = [string[j], string[i]];
-    }
-    selectedElement.textContent = string.join("");
-}
+    try {
+        let string = selectedElement.textContent.split("");
+        for (let i = string.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [string[i], string[j]] = [string[j], string[i]];
+        }
+        selectedElement.textContent = string.join("");
+    } finally { }
 
-function init() {
-    let everyElement = document.querySelectorAll("*")
-    everyElement.forEach((element) => {
-            if (
-                element.textContent.length > 1 && element.children.length == 0 &&
-                element.tagName != "input" && element.tagName != "textarea" &&
-                !element.isContentEditable
-            ) {
-                element.addEventListener("click", () => {
-                    selectedElement = element;
-                })
-            }
-    })
 }
 
 document.addEventListener("keydown", (event) => {
     if (
         event.key == shuffleKey &&
         event.ctrlKey == false && event.shiftKey == false && event.metaKey == false && event.altKey == false
-        ) shuffle();
+    ) shuffle();
 })
 
 let thisSiteBanned = false;
 for (i = 0; i < bannedSites.length; i++) {
     if (window.location.host == bannedSites[i]) thisSiteBanned = true;
 }
-if (!thisSiteBanned) init();
+
+if (!thisSiteBanned) {
+    document.addEventListener("click", (e) => {
+        let target = e.target;
+        if (
+            target.textContent.length > 1 && target.children.length == 0 &&
+            target.tagName != "input" && target.tagName != "textarea" &&
+            !target.isContentEditable
+        ) {
+            selectedElement = target;
+        }
+    })
+}
